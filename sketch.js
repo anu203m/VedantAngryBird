e1e9e789aab25ec53cfea18bbe5305256a375cd1
anu203m <manuradha08@gmail.com>
2021-08-20 12:25:57 +0530

    logmodified

--- sketch.js
+++ sketch.js
@@ -31,9 +31,9 @@ function setup(){
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    log5 = new Log(1170,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(75,75);
 
 }
 
